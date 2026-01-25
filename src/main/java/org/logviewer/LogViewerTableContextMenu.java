b346fd4d8e7c40c27b9d39d719fa9dbb3f257d58
Stephan Droc <stephan.droc@gmail.com>
2026-01-25 20:13:08 +0100

    feat(3): See stack trace on error logs

--- src/main/java/org/logviewer/LogViewerTableContextMenu.java
+++ src/main/java/org/logviewer/LogViewerTableContextMenu.java
@@ -7,14 +7,24 @@ import javax.swing.*;
 @Setter
 public class LogViewerTableContextMenu extends JPopupMenu {
 
+    private final JMenuItem openStack;
     private Object value;
     private Project project;
+
+    public void setStackTrace(String stackTrace) {
+        this.stackTrace = stackTrace;
+        openStack.setVisible(this.stackTrace!=null);
+    }
+
+    private String stackTrace;
 
     LogViewerTableContextMenu() {
         JMenuItem openAsJson = new JMenuItem("Open As Json");
         JMenuItem openAsText = new JMenuItem("Open As Text");
+        openStack = new JMenuItem("Open Stack");
         add(openAsJson);
         add(openAsText);
+        add(openStack);
         openAsJson.addActionListener(actionEvent -> {
             ScratchFileService service = ScratchFileService.getInstance(project);
             service.createAndOpenJsonScratchFile("Scratch",value.toString());
@@ -22,6 +32,10 @@ public class LogViewerTableContextMenu extends JPopupMenu {
         openAsText.addActionListener(actionEvent -> {
             ScratchFileService service = ScratchFileService.getInstance(project);
             service.createAndOpenTextScratchFile("Scratch",value.toString());
+        });
+        openStack.addActionListener(actionEvent -> {
+            ScratchFileService service = ScratchFileService.getInstance(project);
+            service.createAndOpenTextScratchFile("Stack",stackTrace);
         });
     }
 
